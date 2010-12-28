0d10a7aed9cfe95aff1682a4b8ad41707dc6fb80
rohit@xsoftwarelabs.com <rohit@xsoftwarelabs.com@6f584351-6bb5-4940-bf49-539f668a81bf>
2010-12-28 04:39:44 +0000

    initial commit

---
+++ trunk/src/main/java/com/xsoftwarelabs/spring/roo/addon/typicalsecurity/RooTypicalsecurity.java
@@ -0,0 +1,16 @@
+package com.xsoftwarelabs.spring.roo.addon.typicalsecurity;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Trigger annotation
+ * @since 1.1
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface RooTypicalsecurity {
+}
+
