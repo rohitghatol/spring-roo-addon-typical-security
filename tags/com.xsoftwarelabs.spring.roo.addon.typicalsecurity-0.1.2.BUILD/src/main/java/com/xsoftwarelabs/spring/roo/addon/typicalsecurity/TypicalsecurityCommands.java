ee75cc3fc33f967d50b7473d0095d54790cf327c
rohit@xsoftwarelabs.com <rohit@xsoftwarelabs.com@6f584351-6bb5-4940-bf49-539f668a81bf>
2011-01-03 09:38:13 +0000

    [maven-release-plugin]  copy for tag com.xsoftwarelabs.spring.roo.addon.typicalsecurity-0.1.2.BUILD

---
+++ tags/com.xsoftwarelabs.spring.roo.addon.typicalsecurity-0.1.2.BUILD/src/main/java/com/xsoftwarelabs/spring/roo/addon/typicalsecurity/TypicalsecurityCommands.java
@@ -0,0 +1,40 @@
+package com.xsoftwarelabs.spring.roo.addon.typicalsecurity;
+
+import java.util.logging.Logger;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.shell.CliAvailabilityIndicator;
+import org.springframework.roo.shell.CliCommand;
+import org.springframework.roo.shell.CliOption;
+import org.springframework.roo.shell.CommandMarker;
+
+/**
+ * Sample of a command class. The command class is registered by the Roo shell following an
+ * automatic classpath scan. You can provide simple user presentation-related logic in this
+ * class. You can return any objects from each method, or use the logger directly if you'd
+ * like to emit messages of different severity (and therefore different colours on 
+ * non-Windows systems).
+ * 
+ * @since 1.1
+ */
+@Component
+@Service
+public class TypicalsecurityCommands implements CommandMarker {
+	private static Logger logger = Logger.getLogger(TypicalsecurityCommands.class.getName());
+	@Reference private TypicalsecurityOperations operations;
+	
+	@CliAvailabilityIndicator({"Typicalsecurity setup", "Typicalsecurity add"})
+	public boolean isPropertyAvailable() {
+		return operations.isCommandAvailable();
+	}
+	
+	
+	
+	@CliCommand(value = "Typicalsecurity setup", help = "Setup Typicalsecurity addon")
+	public String setup(@CliOption(key = "entityPackage", mandatory = false, help = "entity package",specifiedDefaultValue="~.model",unspecifiedDefaultValue="~.model") String entityPackage,@CliOption(key = "controllerPackage", mandatory = false, help = "controller package",specifiedDefaultValue="~.web",unspecifiedDefaultValue="~.web") String controllerPackage) {
+		return operations.setup( entityPackage,  controllerPackage);
+	}
+}
